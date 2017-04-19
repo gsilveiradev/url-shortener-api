e0e64e976256c4bfa2f9509f648c1b0da7a78bf2
Guilherme Silveira da Silva <guissilveira@gmail.com>
2017-04-19 17:58:09 -0300

    Fix error with incorrect variable.

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () use ($blogPosts) {
+$app->get('/', function () {
     return 'Hello world';
 });
 
