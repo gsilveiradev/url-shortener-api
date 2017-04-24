514f8855e341f7b71abd02d5c5ce352ce98181fa
Guilherme Silveira da Silva <guissilveira@gmail.com>
2017-04-23 22:31:00 -0300

    Create endpoint to create Url for User.

--- src/App/Services/UrlsService.php
+++ src/App/Services/UrlsService.php
@@ -4,5 +4,27 @@ namespace App\Services;
 
 class UrlsService extends BaseService
 {
+	protected $table = "urls";
+	protected $usersTable = "users";
 
+    public function getOneUser($username)
+    {
+        return $this->db->fetchAssoc("SELECT * FROM {$this->usersTable} WHERE username=?", [(string) $username]);
+    }
+
+    public function getOne($id)
+    {
+        return $this->db->fetchAssoc("SELECT * FROM {$this->table} WHERE id=?", [(int) $id]);
+    }
+
+    function save($params)
+    {
+        $this->db->insert($this->table, $params);
+        return $this->db->lastInsertId("urls_id_seq");
+    }
+
+    function update($id, $params)
+    {
+        return $this->db->update($this->table, $params, ['id' => $id]);
+    }
 }
