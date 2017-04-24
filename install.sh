757cecc04bcdfed8a3efbf0587063aed5f0093d2
Guilherme Silveira da Silva <guissilveira@gmail.com>
2017-04-24 14:37:15 -0300

    Adjusted install.sh script.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 cp config.env.example config.env
 docker-compose up -d
-docker-compose exec workspace bash
-composer install
-vendor/bin/doctrine orm:schema-tool:update --force
-vendor/bin/doctrine orm:generate-proxies
-exit
+docker-compose exec workspace bash -c 'composer install'
+docker-compose exec workspace bash -c 'vendor/bin/doctrine orm:schema-tool:update --force'
+docker-compose exec workspace bash -c 'vendor/bin/doctrine orm:generate-proxies'
+exit 1
